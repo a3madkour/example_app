2b25d377485f87c4852a0d636cb14829f43a6548
Abdelrahman Madkour <a3madkour@gmail.com>
2022-03-07 08:28:02 -0500

    Updates

--- src/build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-emcc --bind -s WASM=1 -s MODULARIZE=1 -s EXPORT_ES6=1 -s ENVIRONMENT=web -s USE_BOOST_HEADERS=1 -o quick_example.js  cpp/src/*.cpp -I /usr/include/rapidjson
--- src/cpp/src/LHS.cpp
+++
@@ -1,9 +0,0 @@
-#include "LHS.h"
-
-LHS::LHS(Graph g) : graph(std::move(g)) {}
-LHS::LHS() {}
-
-std::ostream &operator<<(std::ostream &os, const LHS &lhs) {
-  os << lhs.graph;
-  return os;
-}
